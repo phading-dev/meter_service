d4f84b94c73f18593758bbeed7eb124fb1fbb510
Ykuyo <ykuyo.dev@gmail.com>
2025-06-17 20:01:24 -0700

    Seperated bigtable instances between dev and local.

--- dev/env.ts
+++ dev/env.ts
@@ -2,6 +2,7 @@ import "../env_const";
 import "@phading/cluster/dev/env";
 import { ENV_VARS } from "../env_vars";
 
+ENV_VARS.bigtableInstanceId = ENV_VARS.singleBigtableInstanceId;
 ENV_VARS.replicas = 1;
 ENV_VARS.cpu = "400m";
 ENV_VARS.memory = "512Mi";
